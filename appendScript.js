4106aaa460ef278746f300fa73b8dfd2f324828d
Rafael Hernández Larios <rafah.larios@gmail.com>
2019-02-18 12:12:19 -0600

    :rocket: new version

--- appendScript.js
+++ appendScript.js
@@ -5,10 +5,21 @@ const fs = require('fs'),
     env = process.env;
 
 const relativePath = env.npm_package_name;
-const pathFile = `${env.INIT_CWD}/package.json`;
+const pathFile = `${env.INIT_CWD}/package2.json`;
+let packageFile;
 
-const content = require(pathFile);
-// if(content.scripts)
+function appendScript() {
+    try {
+        packageFile = require(pathFile);
+        packageFile.scripts.test = `sh node_modules/${relativePath}/localtest.sh $env`;
+        fs.writeFileSync(pathFile, JSON.stringify(packageFile));
+    } catch (err) {
+        if (err.code === 'MODULE_NOT_FOUND') {
+            packageFile = require('./src/package.json');
+            packageFile.scripts.test = `sh node_modules/${relativePath}/localtest.sh $env`;
+            fs.writeFileSync(pathFile, JSON.stringify(packageFile));
+        } else console.log(err);
+    }
+}
 
-content.scripts.test = `sh node_modules/${relativePath}/localtest.sh $env`;
-fs.writeFileSync(pathFile, JSON.stringify(content));
+appendScript();
